fab3bb384109109655cc44eac6cd378f9648805c
Luis Carpio <luisdcm18@gmail.com>
2020-07-29 01:20:37 -0500

    Avances en mi rama

--- src/main/java/com/mycompany/compraverymaven/dao/TrabajadorImp.java
+++ src/main/java/com/mycompany/compraverymaven/dao/TrabajadorImp.java
@@ -1,5 +1,19 @@
 package com.mycompany.compraverymaven.dao;
+import com.mycompany.compraverymaven.biblioteca.*;
+import com.mycompany.compraverymaven.dto.*;
 
 public class TrabajadorImp implements DaoTrabajador{
+    private final Conexion conecta;
     
+    public TrabajadorImp(){
+        this.conecta=new Conexion();
+    }
+    
+    public void registrar_proveedor(Proveedor proveedor) {
+        try{
+           
+        }catch(Exception e){
+            
+        }
+    }
 }
